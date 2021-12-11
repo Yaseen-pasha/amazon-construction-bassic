1ae0efed051a7366cdc1bd520b795401913b0cb9
Yaseen <95761935+Yaseen-pasha@users.noreply.github.com>
2021-12-11 15:09:48 +0530

    Update form_handler.php

--- form_handler.php
+++ form_handler.php
@@ -4,7 +4,7 @@ $visitor_email = $_POST['email'];
 $number = $_POST['number'];
 $message = $_POST['message'];
 
-$email_from = 'info@https://yaseen-pasha.github.io/amazonconstruction/index.html';
+$email_from = 'https://yaseen-pasha.github.io/amazonconstruction/index.html';
 
 $email_subject = 'New Form Submission';
 
